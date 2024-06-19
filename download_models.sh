533df73ff0f5dfdd11ba4de156615be9e2684dea
Samir Salman <samir.salman@libero.it>
2024-06-19 12:15:11 +0200

    dockerfile hotfix

--- download_models.sh
+++ download_models.sh
@@ -1,11 +1,13 @@
+VERSION=v1_2
+
 cd /FBK
 # download models if the directory doesn't contains the same version
 if [ ! -d "models" ]; then
     mkdir models
 fi
-if [ ! -d "models/FBK_data_v1_2" ]; then
-    wget -O models/FBK_data_v1_2.tar.gz https://fbk.sharepoint.com/:u:/s/MTUnit/$SHAREPOINT_ID?download=1 && tar xvfz models/FBK_data_v1_2.tar.gz && rm models/FBK_data_v1_2.tar.gz
+if [ ! -d "models/FBK_data_$VERSION" ]; then
+    wget -O models/FBK_data_$VERSION.tar.gz https://fbk.sharepoint.com/:u:/s/MTUnit/$SHAREPOINT_ID?download=1 && tar xvfz models/FBK_data_$VERSION.tar.gz && rm models/FBK_data_$VERSION.tar.gz
 fi
-echo "FBK_data_v1_2 downloaded"
+echo "FBK_data_$VERSION downloaded"
 
-mv models/FBK_data_v1_2/* root/.cache
+mv models/FBK_data_$VERSION/* root/.cache
