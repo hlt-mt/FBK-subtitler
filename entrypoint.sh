25d012b95e0d4c48267b1c182580c2cf4456d3e8
Samir Salman <samir.salman@libero.it>
2024-06-26 18:29:03 +0200

    fixed check entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,12 @@ cd /FBK
 if [ ! -d "models" ]; then
     mkdir models
 fi
-if [ ! -d "models/FBK_data_$VERSION" ]; then
-    wget -O models/FBK_data_$VERSION.tar.gz https://fbk.sharepoint.com/:u:/s/MTUnit/$SHAREPOINT_ID?download=1 && tar xvfz models/FBK_data_$VERSION.tar.gz && rm models/FBK_data_$VERSION.tar.gz && mv FBK_data FBK_data_$VERSION && mv FBK_data_$VERSION/.cache/* /root/.cache
+if [ ! -d "/root/.cache/version.txt" ]; then
+    downloaded_version=$(cat /root/version.txt)
+    if [ "$downloaded_version" != "$VERSION" ]; then
+        wget -O models/FBK_data_$VERSION.tar.gz https://fbk.sharepoint.com/:u:/s/MTUnit/$SHAREPOINT_ID?download=1 && tar xvfz models/FBK_data_$VERSION.tar.gz && rm models/FBK_data_$VERSION.tar.gz && mv FBK_data FBK_data_$VERSION && mv FBK_data_$VERSION/.cache/* /root/.cache
+        echo $VERSION >/root/version.txt
+    fi
 fi
 echo "FBK_data_$VERSION downloaded"
 echo "Creating upload and out directories"
