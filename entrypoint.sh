89e94e4a0a30dade93610de4ebb17a024601dc14
Samir Salman <samir.salman@libero.it>
2024-07-01 12:01:55 +0200

    fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,11 @@ if [ ! -d "models" ]; then
     mkdir models
 fi
 if [ ! -d "/root/.cache/version.txt" ]; then
-    downloaded_version=$(cat /root/version.txt)
+    downloaded_version=$(cat /root/.cache/version.txt)
     if [ "$downloaded_version" != "$VERSION" ]; then
+        rm -r /root/.cache/*
         wget -O models/FBK_data_$VERSION.tar.gz https://fbk.sharepoint.com/:u:/s/MTUnit/$SHAREPOINT_ID?download=1 && tar xvfz models/FBK_data_$VERSION.tar.gz && rm models/FBK_data_$VERSION.tar.gz && mv FBK_data FBK_data_$VERSION && mv FBK_data_$VERSION/.cache/* /root/.cache
-        echo $VERSION >/root/version.txt
+        echo $VERSION >/root/.cache/version.txt
     fi
 fi
 echo "FBK_data_$VERSION downloaded"
